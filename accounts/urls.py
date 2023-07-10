ca28d59e95191c93a5cd13c7ebb5c29c56bb0a2a
Momen <momenprogrammer@gmail.com>
2023-07-10 07:50:48 +0300

    Add new app

---
+++ accounts/urls.py
@@ -0,0 +1,2 @@
+from django.urls import path, include
+from accounts import views
--- django_project/urls.py
+++ django_project/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('base.urls')),
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
