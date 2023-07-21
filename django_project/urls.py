cfd2663b464e0ad0b3a0984ccdd28ae16f9810a7
Momen <momenprogrammer@gmail.com>
2023-07-21 11:54:21 +0300

    Add the accounts app

--- django_project/urls.py
+++ django_project/urls.py
@@ -5,9 +5,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('subjects.urls', namespace='accounts')),
     path('', include('courses.urls', namespace='courses')),
     path('', include('subjects.urls', namespace='subjects')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
 ]
 
