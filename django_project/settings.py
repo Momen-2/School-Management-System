91a976a29912e0bc5f9ccd838d23232abc3a6b2f
Momen <momenprogrammer@gmail.com>
2023-07-22 07:55:04 +0300

    Use the django-phonenumber-field Package

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'phonenumber_field',
     'crispy_forms',
     'crispy_bootstrap5',
     'accounts',
