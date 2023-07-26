e466f6aefd207c305daa06ec264f8064a57418c0
Momen <momenprogrammer@gmail.com>
2023-07-26 19:14:14 +0300

    Add dashboards URLs

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -4,5 +4,7 @@ from dashboards import views
 app_name = 'dashboards'
 
 urlpatterns = [
-    
+    path('admin-dashboard', views.AdminDashboardTemplateView.as_view(), name='admin-dashboard'),
+    path('teacher-dashboard', views.TeacherDashboardTemplateView.as_view(), name='teacher-dashboard'),
+    path('student-dashboard', views.StudentDashboardTemplateView.as_view(), name='student-dashboard')
 ]
