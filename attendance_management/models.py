65a107d375ab0c9f528dceee0d027625f43ab346
Momen <momenprogrammer@gmail.com>
2023-08-01 00:54:06 +0300

    Delete attendence_management app

--- attendance_management/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from accounts.models import CustomUser
-
-class Attendance(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
--- attendance_management/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from attendance_management import views
-
-app_name = 'attendance_management'
-
-urlpatterns = [
-
-]
