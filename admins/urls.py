aadb0900d405a900d52f188b9a43bb9805f96db7
Momen <momenprogrammer@gmail.com>
2023-08-01 16:46:56 +0300

    Add an app for admins

---
+++ admins/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from admins import views
+
+app_name = 'admins'
+
+urlpatterns = [
+    path('admin-dashboard', views.AdminDashboardTemplateView.as_view(), name='dashboard'),
+]
