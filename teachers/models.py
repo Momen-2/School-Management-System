8e5a5f38527df5f17357f1bf797a4537960138a3
Momen <momenprogrammer@gmail.com>
2023-08-05 01:17:55 +0300

    Use the post_save signal to create a teacher automatic

--- teachers/models.py
+++ teachers/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from accounts.models import CustomUser
 from classes.models import Class
 
 class Teacher(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     class_relation = models.ManyToManyField(Class)
+    
+@receiver(post_save, sender=CustomUser)
+def create_teacher(sender, instance, created, **kwargs):
+    if created and instance.user_type == 'TEACHER':
+        Teacher.objects.create(user=instance)
