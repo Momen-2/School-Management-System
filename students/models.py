5bb9be4c74d185c155519f97fba2e00ece757a07
Momen <momenprogrammer@gmail.com>
2023-08-05 01:17:01 +0300

    Add the models of students app

--- students/models.py
+++ students/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from accounts.models import CustomUser
+from classes.models import Class
 
-# Create your models here.
+class Student(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    class_relation = models.ForeignKey(Class, on_delete=models.CASCADE, blank=True, null=True)
+    
+@receiver(post_save, sender=CustomUser)
+def create_teacher(sender, instance, created, **kwargs):
+    if created and instance.user_type == 'STUDENT':
+        Student.objects.create(user=instance)
