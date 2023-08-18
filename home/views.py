8583dcd476e12bc08d9670806fc71e3379e272cb
Momen <momenprogrammer@gmail.com>
2023-08-18 16:39:00 +0300

    Solve an issue

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import Group
 
 def home(request):
-    if request.user.is_authenticated and request.user.approve and Group.objects.filter(user=request.user).exists():
+    if request.user.is_authenticated and request.user.is_approved and Group.objects.filter(user=request.user).exists():
         return redirect('accounts:sign-up-successful')
     
     return render(request,'home/home.html')
